401216bbfee52b0fc9f4a0fd8a4aff4f3650af55
Nciae-Zyh <2681904978@qq.com>
2022-05-22 12:58:06 +0800

    免支付环境的支付模式操作。

--- src/main/java/com/example/printserver/service/impl/PayServiceDemo.java
+++ src/main/java/com/example/printserver/service/impl/PayServiceDemo.java
@@ -30,12 +30,19 @@ public class PayServiceDemo implements PayService {
     @Autowired
     Sockets sockets;
 
+    /**
+     * 支付订单
+     *
+     * @param oid 订单编号
+     * @return 支付状态
+     */
     @Override
     public CommonResult payOrder(Integer oid) {
         Order order = orderMapper.selectById(oid);
         Timestamp orderTime = order.getOrderTime();
         Date date = new Date();
         Timestamp timestamp = new Timestamp(date.getTime());
+        // 此方法为判断订单支付状态是否为第一次提交，若第一次提交添加订单时间，若不是则判断是否超时，防止支付失败。
         if (orderTime == null) {
             order.setOrderTime(timestamp);
             orderMapper.updateById(order);
@@ -50,11 +57,21 @@ public class PayServiceDemo implements PayService {
         return CommonResult.success("成功支付（大概吧）", "无支付测试");
     }
 
+    /**
+     * 完成订单时的操作
+     * 该操作逻辑为从sockets取出一个socket用于传输文件，印打印端采用python书写，所以用最基础的BufferedWriter来传输数据。
+     * 同时在打印端续每次一给打印时向表格内添加信息，所以采用中断连接结束进程的方式通过pool获取返回值
+     *
+     * @param out_tar_no 订单编号
+     */
     @Override
     public void finishPayment(String out_tar_no) {
         Integer oid = Integer.valueOf(out_tar_no);
-        Order order = orderMapper.selectById(oid);
-        order.setPayState(1);
+        Order order = orderMapper.selectById(oid);  //在数据库中取出该订单
+        order.setPayState(1);   //将订单状态修改为已支付
+        /**
+         * 新建线程用于传输订单信息给打印端
+         */
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -74,14 +91,18 @@ public class PayServiceDemo implements PayService {
                         }
                         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        // 将文件传输起始标识符输入
                         bufferedWriter.write("1");
                         bufferedWriter.flush();
+                        // 防粘包
                         bufferedReader.read();
+                        // 传输订单号
                         bufferedWriter.write(String.valueOf(oid));
                         bufferedWriter.flush();
                         bufferedReader.read();
                         bufferedWriter.close();
                         bufferedReader.close();
+                        // 关闭本次socket连接
                         socket.close();
                     }
                 } catch (Exception e) {
